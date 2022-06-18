fbb420b83369f683f8ff0252621501acbc44539b
Magdaleno <magdaleno0100@gmail.com>
2022-06-18 00:20:14 -0500

    Add project base

---
+++ GuardClauseHomemade/GuardClauseHomemade/Guard/GuardClauses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace GuardClauseHomemade.Guard
+{
+    internal class GuardClauses
+    {
+        public bool EvaluateInput<T>(Func<T, bool> condicion, string message, T data)
+        {
+            return condicion(data) ? true : throw new Exception(message);
+        }
+    }
+}
---
+++ GuardClauseHomemade/GuardClauseHomemade/Program.cs
@@ -0,0 +1,32 @@
+﻿using GuardClauseHomemade.Guard;
+using System;
+
+namespace GuardClauseHomemade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Input data
+            string name = "Magdaleno";
+            int age = 1;
+
+            try
+            {
+                var oGuardClauses = new GuardClauses();
+                oGuardClauses.EvaluateInput<string>((x) => !string.IsNullOrEmpty(x), "Es nullo o vacío", name);
+                oGuardClauses.EvaluateInput<int>((x) => x > 18, "No es mayor a 18", age);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finaliza Pruebas");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
